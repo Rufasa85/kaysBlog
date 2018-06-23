a90ecf370e434f1095e2cf693926bbf2b1ff450d
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-06-22 20:35:28 -0700

    adding database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./models')
 
 app.set('view engine', 'ejs');
 
@@ -7,6 +8,6 @@ app.use('/', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
 	res.render('index')
-})
+});
 
 app.listen(process.env.PORT || 3000);
