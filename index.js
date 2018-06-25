6a2943009d598153b0cedc01e12d621d748b6961
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-06-25 12:22:53 -0700

    databasing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const db = require('./models')
+const cloudinary = require('cloudinary');
+const apiKeys = require('./static/js/apikeys.js');
 
 app.set('view engine', 'ejs');
 
 app.use('/', express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({extended:false}));
+
+cloudinary.config({
+	cloud_name:apiKeys.cloudName,
+	api_key:apiKeys.apiKey,
+	api_secret:apiKeys.apiSecret
+})
 
 app.get('/', function(req, res) {
 	res.render('index')
@@ -17,7 +25,19 @@ app.get('/new',function(req,res) {
 });
 
 app.post('/', function(req,res) {
-	res.send(req.body);
+	// if (req.body.picone) {
+	// 	cloudinary.uploader.upload
+	// }
+	db.post.create({
+		title:req.body.title,
+		post:req.body.body,
+		picone:req.body.picOne,
+		pictwo:req.body.picTwo,
+		date:new Date()
+	}).then(function(post) {
+		res.send(post);
+	})
+	// res.send(req.body);
 })
 
 app.listen(process.env.PORT || 3000);
